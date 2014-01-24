9c5556c24d64de1fcf3fc3718fe3eea6c14cae0a
J. Eike von Seggern <eikevons@yahoo.de>
2014-01-24 13:25:56 +0100

    clean-up hybrid resampling implementation
    - use better function names
    - add confidence interval function

--- hybrid.py
+++ hybrid.py
@@ -1,36 +1,46 @@
+"""
+Unified confidence limits for a Poissonian signal + *unknown* background
+estimated by hybrid resampling.
+
+This implementation follows [Sen+Walker+Woodroofe. (2009) Stat.Sin.(19) 301--314]
+"""
+# Use float division
+from __future__ import division
 import numpy as np
 
-from tools import conservative_quantile
+from tools import conservative_quantile, bisect
 
 
-def b_hat(n, m, g):
-    return (m + n - t_hat(n, m, g)) / (1.0 + g)
+def global_fit_b(n, m, gamma):
+    return (m + n - global_fit_theta(n, m, gamma)) / (1.0 + gamma)
 
-def t_hat(n, m, g):
-    return np.fmax(0.0, n - m/g)
+def global_fit_theta(n, m, gamma):
+    return np.fmax(0.0, n - m/gamma)
 
-def b_hathat(n, m, t, g):
-    return (m + n - (1+g)*t + np.sqrt( ((1+g)*t - m - n)**2 + 4*(1+g)*m*t )) / (2 * (1+g))
-
-
-def likelihood_ratio(n, m, t, g):
-    bh = b_hat(n, m, g)
-    th = t_hat(n, m, g)
-    bhh = b_hathat(n, m, t, g)
-
-    return (bhh/bh)**m * ((t+bhh)/(th+bh))**n * np.exp(n + m - (1+g)*bhh - t)
+def local_fit_b(n, m, theta, gamma):
+    return (m + n - (1+gamma)*theta + np.sqrt( ((1+gamma)*theta - m - n)**2 + 4*(1+gamma)*m*theta )) / (2 * (1+gamma))
 
 
-def c_t(n, m, t, g, clvl, N_mc=10000):
-    bhh = b_hathat(n, m, t, g)
+def likelihood_ratio(n, m, theta, gamma):
+    bh = global_fit_b(n, m, gamma)
+    th = global_fit_theta(n, m, gamma)
+    bhh = local_fit_b(n, m, theta, gamma)
 
-    ns = np.random.poisson(t + bhh, size=N_mc)
-    ms = np.random.poisson(g*bhh, size=N_mc)
-    l = likelihood_ratio(ns, ms, t, g)
-    # return np.percentile(l, 100*a)
-    return conservative_quantile(l, -(1.0 - clvl)
+    return (bhh/bh)**m * ((theta+bhh)/(th+bh))**n * np.exp(n + m - (1+gamma)*bhh - theta)
+
+
+def critical_value(n, m, theta, gamma, clvl, N_mc):
+    bhh = local_fit_b(n, m, theta, gamma)
+
+    ns = np.random.poisson(theta + bhh, size=N_mc)
+    ms = np.random.poisson(gamma*bhh, size=N_mc)
+    l = likelihood_ratio(ns, ms, theta, gamma)
+    return conservative_quantile(l, -(1.0 - clvl))[0]
 
 def mk_plot(ts, N_mc):
+    """
+    Reproduce Fig.4.2 from [Sen+Walker+Woodroofe. (2009) Stat.Sin.(19) 301--314]
+    """
     import pylab as P
     c_ts = np.empty_like(ts)
     ls = np.empty_like(ts)
@@ -41,7 +51,7 @@ def mk_plot(ts, N_mc):
     a = 0.10
 
     for i, t in enumerate(ts):
-        c_ts[i] = c_t(n, m, t, g, a, N_mc)
+        c_ts[i] = critical_value(n, m, t, g, a, N_mc)
         ls[i] = likelihood_ratio(n, m, t, g)
     P.plot(ts, c_ts, "r-")
     P.plot(ts, ls, "b-")
@@ -49,10 +59,37 @@ def mk_plot(ts, N_mc):
     P.ylabel("$\\Lambda_\\theta$ and $c_\\theta$")
     P.show()
 
+def confidence_interval(n, m, gamma, clvl, N_mc):
+    theta_best = global_fit_theta(n, m, gamma)
 
-if __name__ == "__main__":
-    # print("Comparison with numerical values in [http://www.stat.columbia.edu/~bodhi/research/UnifiedCI.pdf ; Fig.4.2]")
-    # print(c_t(0, 23, 0.82, 6.0, 0.1))
-    # print(likelihood_ratio(0, 23, 0.82, 6.0))
-    mk_plot(np.linspace(0.0, 2.5, 2501), 10000)
+    cache = {}
+    def f(theta, n_mc):
+        k = (theta, n_mc)
+        if k not in cache:
+            ret =  likelihood_ratio(n, m, theta, gamma) - critical_value(n, m, theta, gamma, clvl, n_mc)
+            cache[k] = ret
+        else:
+            ret = cache[k]
+        return ret
+
+    t0 = 0.0
+    if theta_best > 0 and f(0, N_mc) < 0:
+        t0 = bisect(f, 0, theta_best, args=(N_mc,))
+
+    u = theta_best
+    v = max(1, 2*u)
+    while f(v, N_mc) > 0:
+        u = v
+        v = 2*u
+
+    t1 = bisect(f, v, u, args=(N_mc,))
+    return t0, t1
+
+
+
+# if __name__ == "__main__":
+    # # print("Comparison with numerical values in [http://www.stat.columbia.edu/~bodhi/research/UnifiedCI.pdf ; Fig.4.2]")
+    # # print(c_t(0, 23, 0.82, 6.0, 0.1))
+    # # print(likelihood_ratio(0, 23, 0.82, 6.0))
+    # mk_plot(np.linspace(0.0, 2.5, 2501), 10000)
 
