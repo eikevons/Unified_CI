024c0c46f2dcacf9b5fb06d8e54164e82ddc270c
Eike von Seggern <eikevons@yahoo.de>
2014-03-13 14:10:24 +0100

    remove duplicate test code

--- confidencelevel/gaussian.py
+++ confidencelevel/gaussian.py
@@ -142,42 +142,6 @@ def unconstrained_ci(measured, sigma, cl):
     ci = GaussianUnconstrainedInterval(measured, sigma, cl)
     return ci.confidence_interval()
 
-def test_unconstrained():
-    import pylab as P
-    measured = 10.0
-    sigma = 9.0
-
-    alpha = 0.01
-
-    # simple case with no constraints
-    ll = stats.norm.ppf(0.5*alpha, loc=measured, scale=sigma)
-    ul = stats.norm.isf(0.5*alpha, loc=measured, scale=sigma)
-
-    x = np.linspace(measured - 2 * (measured - ll), measured + 2 * (ul - measured))
-    P.plot(x, stats.norm.pdf(x, loc=measured, scale=sigma), label='normal pdf')
-    P.axvline(ll, ls='-', c='k', label='expected CI boundaries')
-    P.axvline(ul, ls='-', c='k')
-
-    confint = GaussianUnconstrainedInterval(measured=measured, sigma=sigma, cl=1.0-alpha)
-    ll_int, ul_int = confint.confidence_interval()
-    P.axvline(ll_int, ls='--', c='r', label='calculated CI boundaries')
-    P.axvline(ul_int, ls='--', c='r')
-
-    P.plot(x, confint.likelihood_ratio(x), '--m', label='likelihood ratio')
-    # P.plot(x, confint.critical_value(x), '--m', label='crit. LR')
-    P.axhline(confint.critical_value(x), ls=':', c='b', label='crit. LR')
-
-    # xi = stats.norm.isf(0.5*alpha)
-    # crit_al = np.exp(-0.5 * xi**2)
-    # P.axhline(crit_al, ls=':', c='b', label='crit. LR')
-
-    P.legend(loc='best')
-    P.xlabel('observable or mean')
-    P.ylabel('pdf or likelihood ratio')
-
-    P.show()
-
-
 class GaussianUnifiedInterval(GaussianUnconstrainedInterval):
     """
     Estimate confidence intervals for mean of a Normal distributed observable
