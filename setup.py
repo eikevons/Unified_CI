2bc31bb0ea8c47cc5c40ed5ab18db783d2548c26
Eike von Seggern <eikevons@yahoo.de>
2015-02-05 14:02:40 +0100

    add README and setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="Unified_CI",
+      description="Calculation of Unified Confidence Intervals",
+      author="Jan Eike von Seggern",
+      author_email="eikevons@yahoo.de",
+      url="http://github.com/eikevons/Unified_CI",
+      packages=["unified_ci"]
+      )
